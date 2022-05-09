6bc87ac58d626852e781c913c4831a82f7f890ff
BlueBuffalos <71950570+BlueBuffalos@users.noreply.github.com>
2022-05-09 08:51:18 -0500

    Add files via upload

---
+++ index.js/orders.js
@@ -0,0 +1,43 @@
+const Orders = [{
+        productName: 'Blood Pressure Device',
+        productNumber: '85631',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Pulse Oximeter Device',
+        productNumber: '36378',
+        paymentStatus: 'Defective',
+        shipping: 'Declined'
+    },
+    {
+        productName: 'Glucose Monitor',
+        productNumber: '49347',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Glucometer Re-supply Kit',
+        productNumber: '96996',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    },
+    {
+        productName: 'Pulse Oximeter',
+        productNumber: '22821',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    },
+    {
+        productName: 'Blood Pressure Device',
+        productNumber: '81475',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Glucometer',
+        productNumber: '00482',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    }
+]
